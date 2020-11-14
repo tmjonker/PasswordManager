dca356b9ab4253a49268d826ad63d8b7da54fbb7
Tim Jonker <tmjonker1@outlook.com>
2020-11-14 13:14:02 -0500

    Updated random password generator

--- src/main/java/com/tmjonker/passwordmanager/generator/PasswordGenerator.java
+++ src/main/java/com/tmjonker/passwordmanager/generator/PasswordGenerator.java
@@ -6,7 +6,9 @@ public class PasswordGenerator {
 
     // 0-9 are included twice to increase likelihood of a numbers being included in generated password.
     private static final String CHARACTERS =
-            "0123456789!@&$abcdefghijklmnopqrstuvwxyz!@&$ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@&$";
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String SPECIAL_CHARACTERS =
+            "!@$&*-_";
 
     public static String generatePassword() {
 
@@ -15,12 +17,19 @@ public class PasswordGenerator {
         String randomCharacter;
 
         for (int i = 0; i < 20; i++) {
-                int randomInt = random.nextInt(CHARACTERS.length());
-                randomCharacter = CHARACTERS.substring(randomInt, randomInt+1);
 
-                password.append(randomCharacter);
+            int randomInt;
+            //Generates a password that contains a random symbol every 5 characters.
+            if (i % 5 == 0 && i != 0) {
+                randomInt = random.nextInt(SPECIAL_CHARACTERS.length());
+                randomCharacter = SPECIAL_CHARACTERS.substring(randomInt, randomInt + 1);
+            } else {
+                randomInt = random.nextInt(CHARACTERS.length());
+                randomCharacter = CHARACTERS.substring(randomInt, randomInt + 1);
             }
 
+            password.append(randomCharacter);
+        }
         return password.toString();
     }
 }
